0c6f2c103e2e3f648f25b95288d27a072ea10332
Bochcio88 <bochcio685@gmail.com>
2019-11-20 06:18:21 +0100

    Konfiguracja

--- Startup.cs
+++ Startup.cs
@@ -35,13 +35,8 @@ namespace GliwickiDzik
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
