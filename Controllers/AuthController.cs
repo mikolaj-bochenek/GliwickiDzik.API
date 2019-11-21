33e061a2df5066f102775f3c93850019563766d9
Bochcio88 <bochcio685@gmail.com>
2019-11-21 12:36:16 +0100

    Implementacja AuthController

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,7 @@
+using System.Threading.Tasks;
 using GliwickiDzik.Data;
+using GliwickiDzik.DTOs;
+using GliwickiDzik.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GliwickiDzik.Controllers
@@ -13,6 +16,37 @@ namespace GliwickiDzik.Controllers
         public AuthController(IAuthRepository repository)
         {
             _repository = repository;
+        }
+
+        // http:localhost/api/auth/register
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDTO userForRegisterDTO)
+        {
+            userForRegisterDTO.Username = userForRegisterDTO.Username.ToLower();
+
+            if(await _repository.IsUserExist(userForRegisterDTO.Username))
+                return BadRequest("Użytkownik o podanym loginie juz istnieje");
+            
+            var userToCreate = new User
+            {
+                Username = userForRegisterDTO.Username
+            };
+
+            var createdUser = await _repository.Register(userToCreate, userForRegisterDTO.Password);
+
+            return StatusCode(200);
+        }
+
+        // http:localhost/api/auth/login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDTO userForLoginDTO)
+        {
+            var userToLogin = await _repository.Login(userForLoginDTO.Username, userForLoginDTO.Password);
+
+            if(userToLogin == null)
+                return Unauthorized();
+            
+            return StatusCode(200);
         }
     }
 }
