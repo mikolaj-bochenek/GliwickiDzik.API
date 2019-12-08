d37c44107da253af9be016d36494ffdebc15fa43
Bochcio88 <bochcio685@gmail.com>
2019-12-08 13:57:19 +0100

    Add ExerciseForTrainingModel

---
+++ Models/ExerciseForTrainingModel.cs
@@ -0,0 +1,9 @@
+namespace GliwickiDzik.API.Models
+{
+    public class ExerciseForTrainingModel
+    {
+        public int Id { get; set; }
+        public int Sets { get; set; }
+        public int Reps { get; set; }
+    }
+}
