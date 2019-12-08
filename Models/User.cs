81d5d1cad89ddafc7dd092cc48c3c2cfdf423322
Bochcio88 <bochcio685@gmail.com>
2019-12-08 11:05:20 +0100

    User model extension

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,26 @@
+using System;
+
 namespace GliwickiDzik.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        //For Register
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        //For Information
+        public DateTime DateOfCreated { get; set; }
+        public DateTime LastActive { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public float Growth  { get; set; }
+        public float Weight { get; set; }
+        public string Description { get; set; }
     }
 }
