7a77e17ab5c89d15cf8ada4ee27671d784db01d6
Bochcio88 <bochcio685@gmail.com>
2019-12-10 19:19:52 +0100

    AddApplicationError in class Extensions

--- Startup.cs
+++ Startup.cs
@@ -22,6 +22,7 @@ using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
+using GliwickiDzik.API.Helpers;
 
 namespace GliwickiDzik
 {
