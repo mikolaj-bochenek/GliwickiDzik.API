5d42c15b6fa49f1b5cbe335aee96f81bc9f203de
Bochcio88 <bochcio685@gmail.com>
2019-12-10 21:06:31 +0100

    Edit UserForEditDTO

--- DTOs/UserForEditDTO.cs
+++ DTOs/UserForEditDTO.cs
@@ -4,8 +4,6 @@ namespace GliwickiDzik.API.DTOs
 {
     public class UserForEditDTO
     {
-        public int Id { get; set; }
-        public string Username { get; set; }
         public string Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string City { get; set; }
