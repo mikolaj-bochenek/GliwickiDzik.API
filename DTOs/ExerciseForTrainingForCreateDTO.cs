64e3c396eff60396dac3c7cfa9fedc222e1521dc
Bochcio88 <bochcio685@gmail.com>
2019-12-18 09:14:42 +0100

    Cretate ExerciseForTrainingForCreateDTO

---
+++ DTOs/ExerciseForTrainingForCreateDTO.cs
@@ -0,0 +1,9 @@
+namespace GliwickiDzik.API.DTOs
+{
+    public class ExerciseForTrainingForCreateDTO
+    {
+        public string Name { get; set; }
+        public int Sets { get; set; }
+        public int Reps { get; set; }
+    }
+}
