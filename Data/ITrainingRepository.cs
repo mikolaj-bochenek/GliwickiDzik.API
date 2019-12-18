6f7e51548df37cd673928879a4d9a96c9d2bf8ce
Bochcio88 <bochcio685@gmail.com>
2019-12-18 08:57:59 +0100

    ITrainingRepository extension

--- Data/ITrainingRepository.cs
+++ Data/ITrainingRepository.cs
@@ -1,9 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using GliwickiDzik.API.Models;
 
 namespace GliwickiDzik.API.Data
 {
     public interface ITrainingRepository : IGenericRepository<TrainingPlanModel>, IGenericRepository<TrainingModel>, IGenericRepository<ExerciseForTrainingModel>
     {
-         
+         Task<TrainingPlanModel> GetTrainingPlanAsync(int id);
+         Task<IEnumerable<TrainingPlanModel>> GetAllTrainingPlansAsync();
+         Task<TrainingModel> GetTrainingAsync(int id);
+         Task<IEnumerable<TrainingPlanModel>> GetAllTrainingsAsync();
+         Task<ExerciseForTrainingModel> GetExerciseForTrainingAsync(int id);
+         Task<IEnumerable<ExerciseForTrainingModel>> GetAllExercisesForTrainingAsync();
     }
 }
