f73359fad48d10980d3f459e16bfeab7c42592e5
Bochcio88 <bochcio685@gmail.com>
2019-12-18 09:05:06 +0100

    IUserRepository extension

--- Data/IUserRepository.cs
+++ Data/IUserRepository.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using GliwickiDzik.Models;
 
 namespace GliwickiDzik.API.Data
 {
     public interface IUserRepository : IGenericRepository<UserModel>
     {
-         
+         Task<UserModel> GetUserByIdAsync(int id);
+         Task<IEnumerable<UserModel>> GetAllUsersAsync();
     }
 }
