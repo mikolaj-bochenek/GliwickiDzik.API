9d073145cae11d0cf7a8514d29c59c7483fb1f3e
Bochcio88 <bochcio685@gmail.com>
2019-12-23 17:16:12 +0100

    Create TrainingPlanForReturnDTO

---
+++ DTOs/TrainingPlanForReturnDTO.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using GliwickiDzik.API.Models;
+
+namespace GliwickiDzik.API.DTOs
+{
+    public class TrainingPlanForReturnDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateOfCreate { get; set; }
+        public string Level { get; set; }
+        public bool IsMain { get; set; }
+        public ICollection<TrainingPlanModel> Trainings { get; set; }
+    }
+}
