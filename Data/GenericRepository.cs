1615fe7128cdfb6d23abae8165548d5f3f566ddb
Mikolaj Bochenek <mikolaj.bochenek@gmail.com>
2020-01-30 11:25:25 +0100

    REF - GnericRepository

--- Data/GenericRepository.cs
+++ Data/GenericRepository.cs
@@ -8,33 +8,52 @@ using System;
 
 namespace GliwickiDzik.API.Data
 {
-    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    public class GenericRepository<T>: IGenericRepository<T> where T : class
     {
-        private readonly DataContext _context;
-
+        protected readonly DataContext _dataContext;
         public GenericRepository(DataContext context)
         {
-            _context = context;
+            _dataContext = context;
         }
 
         public void Add(T entity)
         {
-            _context.Set<T>().Add(entity);
+            _dataContext.Set<T>().Add(entity);
         }
 
         public void AddRange(IEnumerable<T> entities)
         {
-            _context.Set<T>().AddRange(entities);
+            _dataContext.Set<T>().AddRange(entities);
+        }
+
+        public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dataContext.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<T> FindOneAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dataContext.Set<T>().Where(predicate).SingleOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dataContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dataContext.Set<T>().FirstOrDefaultAsync();
         }
 
         public void Remove(T entity)
         {
-           _context.Set<T>().Remove(entity);
+            _dataContext.Set<T>().Remove(entity);
         }
 
         public void RemoveRange(IEnumerable<T> entities)
         {
-            _context.Set<T>().RemoveRange(entities);
+            _dataContext.Set<T>().RemoveRange(entities);
         }
     }
 }
--- Data/IGenericRepository.cs
+++ Data/IGenericRepository.cs
@@ -9,6 +9,10 @@ namespace GliwickiDzik.API.Data
 {
     public interface IGenericRepository<T> where T : class
     {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> predicate);
+        Task<T> FindOneAsync(Expression<Func<T, bool>> predicate);
         void Add(T entity);
         void AddRange(IEnumerable<T> entities);
         void Remove(T entity);
